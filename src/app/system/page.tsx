4264c7a70b0ca5c7aaa0e4eee2dfe7e59a1de503
Dylan Vu <dylanvu9@gmail.com>
2024-01-29 18:58:57 -0800

    make scaffold for frontend

---
+++ src/app/system/page.tsx
@@ -0,0 +1,8 @@
+import PasswordInputPage from "@/components/passwordInput";
+import react from "react";
+
+export default function System() {
+    return (
+        <PasswordInputPage codeBlock="system" key="system" />
+    )
+}
---
+++ src/components/passwordInput.tsx
@@ -0,0 +1,17 @@
+import react from "react";
+
+/**
+ * 
+ * @param codeBlock - the title of the page
+ * @param key - the key to send to the backend in the body
+ * @returns 
+ */
+export default function PasswordInputPage(props: { codeBlock: string, key: string }) {
+    return (
+        <div>
+            <h1>
+                {props.codeBlock}
+            </h1>
+        </div >
+    )
+}
