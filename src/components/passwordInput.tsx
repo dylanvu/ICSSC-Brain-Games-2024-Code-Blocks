2bff0fbab19b9bb2734408099a548ec89e4e8bee
audreyyluu <audreyylu04@gmail.com>
2024-02-01 19:14:09 -0800

    created password page, no functionality yet

--- src/components/passwordInput.tsx
+++ src/components/passwordInput.tsx
@@ -8,13 +8,24 @@ import react from "react";
  */
 export default function PasswordInputPage(props: { codeBlock: string, key: string, hint: string }) {
     return (
-        <div>
-            <h1>
-                {props.codeBlock}
+        <main className="min-h-screen p-4 font-mono">
+            <h1 className="bg-lime-800 border border-white rounded-md p-2 whitespace-nowrap max-w-min text-white">
+                Code Blocks
             </h1>
-            <div>
-                {props.hint}
+            <div className="grid place-items-center">
+              <h1 className="bg-white border border-lime-800 rounded-md p-2 whitespace-nowrap max-w-min text-lg text-lime-800">
+                Block: {props.codeBlock}
+              </h1>
+              <br></br><br></br>
+              <div className="bg-white border border-lime-800 rounded-md p-9 grid place-items-center max-w-min">
+                <div className="p-2 text-center text-sm text-lime-800">
+                  Hint: {props.hint}
+                </div>
+                <input placeholder="Input password..." className="border border-lime-800 rounded-md p-2 border"></input>
+                <br></br>
+                <button className="bg-white border border-lime-800 rounded-md p-2 text-lime-800">Submit</button>
+              </div>
             </div>
-        </div >
+        </main>
     )
 }
