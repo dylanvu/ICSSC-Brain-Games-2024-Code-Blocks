4a1558976db5a1a3870630b48db9b5a9987922ca
audreyyluu <audreyylu04@gmail.com>
2024-02-01 21:07:57 -0800

    working on interactivity for input and submit

--- src/components/passwordInput.tsx
+++ src/components/passwordInput.tsx
@@ -1,4 +1,5 @@
-import react from "react";
+import React, { useState } from "react";
+import { useRouter } from 'next/router';
 
 /**
  * 
@@ -7,23 +8,45 @@ import react from "react";
  * @returns 
  */
 export default function PasswordInputPage(props: { codeBlock: string, key: string, hint: string }) {
+    const [password, setPassword] = useState('');
+    const [status, setStatus] = useState('waiting');
+    const router = useRouter();
+
+    const handlePasswordInput = (input) => {
+      setPassword(input.target.value);
+    };
+
+    const handleFormSubmit = async (e) => {
+      e.preventDefault();
+      setStatus('loading');
+    // wip
+    }
+    
     return (
         <main className="min-h-screen p-4 font-mono">
-            <h1 className="bg-lime-800 border border-white rounded-md p-2 whitespace-nowrap max-w-min text-white">
+            <h1 className="bg-lime-800 border border-white border-2 rounded-md p-2 whitespace-nowrap max-w-min text-white">
                 Code Blocks
             </h1>
+
             <div className="grid place-items-center">
-              <h1 className="bg-white border border-lime-800 rounded-md p-2 whitespace-nowrap max-w-min text-lg text-lime-800">
+              <h1 className="bg-white border border-lime-800 border-2 rounded-md p-2 whitespace-nowrap max-w-min text-lg font-bold text-lime-800">
                 Block: {props.codeBlock}
               </h1>
               <br></br><br></br>
-              <div className="bg-white border border-lime-800 rounded-md p-9 grid place-items-center max-w-min">
+              
+              <div className="bg-white border border-lime-800 border-2 rounded-md p-9 grid place-items-center max-w-min">
                 <div className="p-2 text-center text-sm text-lime-800">
                   Hint: {props.hint}
                 </div>
-                <input placeholder="Input password..." className="border border-lime-800 rounded-md p-2 border"></input>
-                <br></br>
-                <button className="bg-white border border-lime-800 rounded-md p-2 text-lime-800">Submit</button>
+
+                <form onSubmit={handleFormSubmit}>
+                  <input value={password} placeholder="Input password..." onChange={handlePasswordInput} 
+                         disabled={status === 'loading'} className="border border-lime-800 rounded-md p-2 border"></input>
+                  <br></br>
+                  
+                  <button type="submit" disabled={status === 'loading'} className="bg-white border border-lime-800 rounded-md p-2 text-lime-800">Submit</button>
+                </form>
+                {status === 'fail' && <p className="text-sm text-red">Incorrect password.</p>}  // will it disappear when the status changes?
               </div>
             </div>
         </main>
