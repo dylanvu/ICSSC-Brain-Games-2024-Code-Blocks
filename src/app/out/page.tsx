263b7fb7bb776b9fa5faad6f3b95520dd349a4cd
Dylan Vu <dylanvu9@gmail.com>
2024-02-02 19:37:41 -0800

    create pages for the necessary code blocks

---
+++ src/app/out/page.tsx
@@ -0,0 +1,8 @@
+import PasswordInputPage from "@/components/passwordInput";
+import react from "react";
+
+export default function Out() {
+    return (
+        <PasswordInputPage codeBlock="out" key="out" hint="Test Hint" />
+    )
+}
