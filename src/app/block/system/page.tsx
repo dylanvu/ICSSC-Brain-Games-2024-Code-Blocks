f6229c99a840a1019be607cc1406e431313c01e5
Dylan Vu <dylanvu9@gmail.com>
2024-02-03 22:30:01 -0800

    fix hints for system

--- src/app/block/system/page.tsx
+++ src/app/block/system/page.tsx
@@ -3,6 +3,6 @@ import react from "react";
 
 export default function System() {
     return (
-        <PasswordInputPage codeBlock="system" blockKey="system" hint="Test Hint" />
+        <PasswordInputPage codeBlock="system" blockKey="system" hint="The name of the dog in the Code Blocks presentation slides" />
     )
 }
