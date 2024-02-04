e4b37e4eecbdf4e0ba1ac8978757703418759c8f
Dylan Vu <75911565+dylanvu@users.noreply.github.com>
2024-02-03 21:19:39 -0800

    Backend password matching (#8)
    * Backend
    Filled out skeleton of route.ts and created .env.local file to store passwords.
    
    * add hello-world password
    
    ---------
    
    Co-authored-by: chaeeul3 <chaeeul3@uci.edu>

--- src/app/api/verify/route.ts
+++ src/app/api/verify/route.ts
@@ -15,4 +15,23 @@ export async function POST(
     { params }: any,
 ): Promise<any> {
 
+    const passwords: Record<string, string> = {
+        "system": process.env.SYSTEM_PASSWORD || "",
+        "out": process.env.OUT_PASSWORD || "",
+        "paren": process.env.PAREN_PASSWORD || "",
+        "quote": process.env.QUOTE_PASSWORD || "",
+        "reversed": process.env.REVERSED_PASSWORD || "",
+        "hello-world": process.env.HELLO_WORLD_PASSWORD || "",
+    };
+
+    const body = await request.json();
+    const key: string = body.key as string;
+    const password = body.password;
+
+    if (passwords[key] && password === passwords[key]) {
+        return NextResponse.json({ status: 200 });
+    }
+    else {
+        return NextResponse.json({ status: 401 });
+    }
 }
