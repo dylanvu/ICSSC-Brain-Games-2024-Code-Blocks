bf32b3a6650ea18fc64ef826469544ee25e0ce02
Dylan Vu <dylanvu9@gmail.com>
2024-02-03 20:58:32 -0800

    create pages for food court and terrace

---
+++ src/app/food-court/page.tsx
@@ -0,0 +1,8 @@
+import BlockListPage from "@/components/blockList";
+
+export default function FoodCourtPage() {
+
+    return (
+        <BlockListPage codeBlocks={[`("Hello World")`]} location="Terrace Stage" />
+    )
+}
