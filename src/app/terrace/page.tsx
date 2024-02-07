1461c7828ed1336faa0222bda8488c3a3da5e36c
Dylan Vu <dylanvu9@gmail.com>
2024-02-06 22:52:50 -0800

    remove terrace and hello-world since it was only for play testing

--- src/app/terrace/page.tsx
+++
@@ -1,8 +0,0 @@
-import BlockListPage from "@/components/blockList";
-
-export default function TerracePage() {
-
-    return (
-        <BlockListPage codeBlocks={["system"]} location="Terrace Stage" />
-    )
-}
