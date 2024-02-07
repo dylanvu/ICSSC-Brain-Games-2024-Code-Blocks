2573498e8d2d1b85ab956248813e0c977e1ad2cb
Dylan Vu <dylanvu9@gmail.com>
2024-02-06 16:53:14 -0800

    escape apostrophe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 text-6xl">
-      Welcome to Brain Games's Code Blocks! Look for clues to code blocks!
+      Welcome to Brain Games&apos; Code Blocks! Look for clues to code blocks!
     </main>
   )
 }
