f81013400d79669993a553c13965bad6ed08dbca
Dylan Vu <dylanvu9@gmail.com>
2024-02-09 20:26:16 -0800

    try to eliminate similar answers through hints

--- src/app/block/reversed/page.tsx
+++ src/app/block/reversed/page.tsx
@@ -8,7 +8,7 @@ import "@/styles/image.css";
 export default function Reversed() {
     return (
         <>
-            <PasswordInputPage codeBlock="reversed" blockKey="reversed" hint="Solve the puzzle below to find the password..." />
+            <PasswordInputPage codeBlock="reversed" blockKey="reversed" hint="Solve the puzzle below to find the password. (answer is singular)" />
             <div className="h-64 w-96 relative m-auto">
                 <Image src={minecraft} alt={"Location Image"} fill style={{ "objectFit": "contain" }} />
             </div>
