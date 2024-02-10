38486bf2181a1b72659aa2aebe1ac0fa7e5ae389
Dylan Vu <dylanvu9@gmail.com>
2024-02-09 16:35:30 -0800

    add all hints and fix mobile formatting issues

--- src/app/block/quote/page.tsx
+++ src/app/block/quote/page.tsx
@@ -3,6 +3,6 @@ import react from "react";
 
 export default function Quote() {
     return (
-        <PasswordInputPage codeBlock={`"`} blockKey="quote" hint="Test Hint" />
+        <PasswordInputPage codeBlock={`"`} blockKey="quote" hint="I am not alive, but I can grow. I don't have lungs, but I need air. What am I? (all lower case)" />
     )
 }
